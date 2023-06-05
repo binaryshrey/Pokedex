ea0e1865629856adbf73eb5dd0923684f8ad75b0
binaryshrey <shreyansh.saurabh2016@vitstudent.ac.in>
2023-06-05 14:54:32 +0530

    updates loginFrag view

--- app/src/main/java/dev/shreyansh/pokemon/pokedex/ui/login/LoginFragment.kt
+++ app/src/main/java/dev/shreyansh/pokemon/pokedex/ui/login/LoginFragment.kt
@@ -5,6 +5,7 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.view.WindowManager
 import android.widget.Toast
 import androidx.databinding.DataBindingUtil
 import dev.shreyansh.pokemon.pokedex.R
@@ -15,8 +16,9 @@ class LoginFragment : Fragment() {
 
     private lateinit var binding : FragmentLoginBinding
 
-    override fun onCreateView( inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
-        // Inflate the layout for this fragment
+    override fun onCreateView( inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View {
+
+        setStatusBarToTransparent()
         binding = DataBindingUtil.inflate(inflater,R.layout.fragment_login, container, false)
         initOnClickListeners()
 
@@ -27,6 +29,10 @@ class LoginFragment : Fragment() {
         binding.privacyTV.setOnClickListener { Toast.makeText(context,"Privacy Policy",Toast.LENGTH_SHORT).show() }
         binding.termsTV.setOnClickListener { Toast.makeText(context,"Terms of Service",Toast.LENGTH_SHORT).show() }
 
+    }
+
+    private fun setStatusBarToTransparent(){
+        requireActivity().window.setFlags(WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS, WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS)
     }
 
 
