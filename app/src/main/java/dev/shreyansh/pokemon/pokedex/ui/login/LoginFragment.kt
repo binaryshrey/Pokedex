53477eee51aa1f7daf56e41df907727f05256952
binaryshrey <shreyansh.saurabh2016@vitstudent.ac.in>
2023-06-05 13:46:15 +0530

    adds onClickListeners for privacy and tos

--- app/src/main/java/dev/shreyansh/pokemon/pokedex/ui/login/LoginFragment.kt
+++ app/src/main/java/dev/shreyansh/pokemon/pokedex/ui/login/LoginFragment.kt
@@ -5,16 +5,28 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
+import androidx.databinding.DataBindingUtil
 import dev.shreyansh.pokemon.pokedex.R
+import dev.shreyansh.pokemon.pokedex.databinding.FragmentLoginBinding
 
 
 class LoginFragment : Fragment() {
 
+    private lateinit var binding : FragmentLoginBinding
 
     override fun onCreateView( inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         // Inflate the layout for this fragment
+        binding = DataBindingUtil.inflate(inflater,R.layout.fragment_login, container, false)
+        initOnClickListeners()
 
-        return inflater.inflate(R.layout.fragment_login, container, false)
+        return binding.root
+    }
+
+    private fun initOnClickListeners() {
+        binding.privacyTV.setOnClickListener { Toast.makeText(context,"Privacy Policy",Toast.LENGTH_SHORT).show() }
+        binding.termsTV.setOnClickListener { Toast.makeText(context,"Terms of Service",Toast.LENGTH_SHORT).show() }
+
     }
 
 
