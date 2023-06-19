01dd4ea3bd88c69d5abccb0d3a4b08751fd0600d
binaryshrey <shreyansh.saurabh2016@vitstudent.ac.in>
2023-06-19 17:58:17 +0530

    adds moves list item

--- app/src/main/java/dev/shreyansh/pokemon/pokedex/network/response/MovesResponse.kt
+++ app/src/main/java/dev/shreyansh/pokemon/pokedex/network/response/MovesResponse.kt
@@ -1,7 +1,6 @@
 package dev.shreyansh.pokemon.pokedex.network.response
 
 import android.os.Parcelable
-import com.squareup.moshi.Json
 import com.squareup.moshi.JsonClass
 import kotlinx.parcelize.Parcelize
 
@@ -11,22 +10,16 @@ import kotlinx.parcelize.Parcelize
 data class MovesResponse(
     val id: Int,
     val name: String,
+    val power: Int,
     val accuracy: Int,
-    @Json(name = "effectEntries") val effect_entries: List<Effect>,
-    @Json(name = "learnedByPokemon") val learned_by_pokemon: List<PokemonLearned>,
+    val effect: String,
+    val learnedByPokemon : List<LearnedByPokemon>,
 ) : Parcelable
 
 
 @JsonClass(generateAdapter = true)
 @Parcelize
-data class Effect(
-    @Json(name = "effect") val short_effect : String
-) : Parcelable
-
-
-@JsonClass(generateAdapter = true)
-@Parcelize
-data class PokemonLearned(
-    val name : String,
-    val url : String,
+data class LearnedByPokemon(
+    val name: String,
+    val url: String,
 ) : Parcelable
