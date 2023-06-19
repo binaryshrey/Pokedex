14d66a9854e0dccacbd4ddcc58e9047291d1c813
binaryshrey <shreyansh.saurabh2016@vitstudent.ac.in>
2023-06-19 13:35:26 +0530

    adds movesService API handler

--- app/src/main/java/dev/shreyansh/pokemon/pokedex/network/MovesService.kt
+++ app/src/main/java/dev/shreyansh/pokemon/pokedex/network/MovesService.kt
@@ -1,4 +1,41 @@
 package dev.shreyansh.pokemon.pokedex.network
 
-class MovesService {
+import com.jakewharton.retrofit2.adapter.kotlin.coroutines.CoroutineCallAdapterFactory
+import com.squareup.moshi.Moshi
+import com.squareup.moshi.kotlin.reflect.KotlinJsonAdapterFactory
+import dev.shreyansh.pokemon.pokedex.network.response.MovesResponse
+import dev.shreyansh.pokemon.pokedex.network.response.PokeNewsRequest
+import retrofit2.Retrofit
+import retrofit2.converter.moshi.MoshiConverterFactory
+import retrofit2.http.GET
+import retrofit2.http.Query
+
+
+
+
+private const val MOVES_BASE_URL = "https://raw.githubusercontent.com/binaryshrey/Pokedex/feat/moves/"
+
+private val moshi = Moshi.Builder()
+    .add(KotlinJsonAdapterFactory())
+    .build()
+
+private val retrofitMoves = Retrofit.Builder()
+    .addConverterFactory(MoshiConverterFactory.create(moshi))
+    .addCallAdapterFactory(CoroutineCallAdapterFactory())
+    .baseUrl(MOVES_BASE_URL)
+    .build()
+
+
+interface MovesService {
+
+    @GET("/moves.json")
+    suspend fun getPokeMoves(): List<MovesResponse>
+
+}
+
+
+object MovesServiceAPI {
+    val movesService: MovesService by lazy {
+        retrofitMoves.create(MovesService::class.java)
+    }
 }
