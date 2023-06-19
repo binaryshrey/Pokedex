7603ef1e09da8adeba18384aa5c35291c8f8a1ca
binaryshrey <shreyansh.saurabh2016@vitstudent.ac.in>
2023-06-19 13:40:26 +0530

    adds moves frag

---
+++ app/src/main/java/dev/shreyansh/pokemon/pokedex/ui/moves/MovesFragment.kt
@@ -0,0 +1,17 @@
+package dev.shreyansh.pokemon.pokedex.ui.moves
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import dev.shreyansh.pokemon.pokedex.R
+
+class MovesFragment : Fragment() {
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+
+        return inflater.inflate(R.layout.fragment_moves, container, false)
+    }
+
+}
