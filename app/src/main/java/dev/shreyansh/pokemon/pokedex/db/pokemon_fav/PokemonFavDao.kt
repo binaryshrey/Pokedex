e94a4367b29d18955147d45c2b7eb3c081b91bec
binaryshrey <shreyansh.saurabh2016@vitstudent.ac.in>
2023-07-11 16:04:37 +0530

    adds fav-pokemon entity, dao and db models

---
+++ app/src/main/java/dev/shreyansh/pokemon/pokedex/db/pokemon_fav/PokemonFavDao.kt
@@ -0,0 +1,22 @@
+package dev.shreyansh.pokemon.pokedex.db.pokemon_fav
+
+import androidx.lifecycle.LiveData
+import androidx.room.*
+
+
+@Dao
+interface PokemonFavDao {
+
+    @Insert(onConflict = OnConflictStrategy.IGNORE)
+    suspend fun insertFav(pokemonFavEntity: PokemonFavEntity)
+
+    @Query("SELECT * FROM pokemon_fav_table")
+    fun getAllFavPokemon(): LiveData<List<PokemonFavEntity>>
+
+    @Delete
+    suspend fun deleteFav(pokemonFavEntity: PokemonFavEntity)
+
+    @Query("DELETE FROM pokemon_fav_table")
+    suspend fun clear()
+
+}
